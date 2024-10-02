61ee1e2a510d098a4a533afea7299376e8189d92
Marina Triviño <maritriv@ucm.es>
2024-10-02 15:49:40 +0200

    Actualizacion

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
+from pathlib import Path
+
+
+class Regla:
+    def __init__(self, cons, resto):
+        self.cons = cons
+		self.resto = resto
+
+
 def main():
+	fichero = Path("base_laboral.txt")
+	texto = fichero.read_text()
+	for line in texto.split("\n"):
+		if line.startswith("#") or not line:
+			continue
+		cons, resto = line.split(" :- ")
+		cons = cons.strip()
+		antecedentes = rest.split(",")
+		print(line)
     print("Hello from sbc-g01-p2!")
 
 
 if __name__ == "__main__":
     main()
+    
