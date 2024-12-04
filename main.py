a93e217651c7f0d169e4729680807f578f9b8757
Aula1008 <maritriv@ucm.es>
2024-12-04 14:47:19 +0100

    A

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-import click
-from utils import iniciar_bucle_interactivo
-from base_conocimiento import cargar_bases_conocimiento
+from ollama import chat
+from ollama import ChatResponse
 
-
-@click.command()
-@click.argument("archivos_base_conocimiento", nargs=-1)  # Acepta uno o más archivos
-def main(archivos_base_conocimiento):
-    """
-    Programa principal para cargar bases de conocimiento y ejecutar consultas.
-
-    ARCHIVOS_BASE_CONOCIMIENTO: Uno o más archivos de base de conocimiento.
-    """
-    base_conocimiento = cargar_bases_conocimiento(archivos_base_conocimiento)
-
-    if base_conocimiento:
-        iniciar_bucle_interactivo(base_conocimiento)
+def main():
+    response: ChatResponse = chat(model='llama3.2:1b', messages=[
+      {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+      },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
 
 
 if __name__ == "__main__":
